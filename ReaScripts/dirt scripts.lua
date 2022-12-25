d6c45d38f3fe01501fcb65974bb41981bfb484fa
Fleeesch <fleeesch@gmail.com>
2022-12-25 21:57:09 +0100

    ok

--- ReaScripts/dirt scripts.lua
+++ ReaScripts/dirt scripts.lua
@@ -3,7 +3,7 @@
 -- @version 1.4
 -- @metapackage
 -- @provides
---    Dirt Scripts/_dirt_functions
+--   Dirt Scripts/_dirt_functions
 --   [main] Dirt Scripts/Fleeesch - Dirt - Load Set 1.lua
 --   [main] Dirt Scripts/Fleeesch - Dirt - Load Set 2.lua
 --   [main] Dirt Scripts/Fleeesch - Dirt - Load Set 3.lua
