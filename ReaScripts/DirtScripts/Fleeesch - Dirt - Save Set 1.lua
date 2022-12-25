b8354a6df7b97d18ab3424fda1eb6b683b717537
Fleeesch <fleeesch@gmail.com>
2022-12-25 21:49:37 +0100

    ok

--- ReaScripts/DirtScripts/Fleeesch - Dirt - Save Set 1.lua
+++ ReaScripts/DirtScripts/Fleeesch - Dirt - Save Set 1.lua
@@ -1,3 +1,4 @@
+-- NoIndex: true
 
 local info = debug.getinfo(1,'S');
 script_path = info.source:match[[^@?(.*[\/])[^\/]-$]]
--- ReaScripts/DirtScripts/Fleeesch - Dirt - Save Set 6.lua
+++ ReaScripts/DirtScripts/Fleeesch - Dirt - Save Set 6.lua
@@ -1,3 +1,5 @@
+-- NoIndex: true
+
 local info = debug.getinfo(1,'S');
 script_path = info.source:match[[^@?(.*[\/])[^\/]-$]]
 dofile(script_path .. "_dirt_functions")
